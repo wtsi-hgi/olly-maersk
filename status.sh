3eeb57c646b2fc48dd48014ec20b609e9d2ed28f
Christopher Harrison <ch12@sanger.ac.uk>
2019-04-25 15:50:08 +0100

    LSF job status function

--- status.sh
+++ status.sh
@@ -22,6 +22,20 @@ usage() {
 	EOF
 }
 
+lfs_job_status() {
+  # Return the status of an LSF job (or NOTFOUND if not found)
+  # n.b., NOTFOUND jobs may be because they've ended and dropped out of
+  # the LSF log rotation (either that or they never existed in the first
+  # place); their actual status may therefore be available using bhist,
+  # but this is costly and also subject to LSF's log rotation.
+  # n.b., This only works correctly for scalar jobs, but that's all
+  # we're dealing with so it's totally fine.
+  local job_id="$1"
+  local status="$(bjobs -noheader -o "stat" "${job_id}" 2>/dev/null)"
+
+  echo "${status:-NOTFOUND}"
+}
+
 main() {
   local -a workflow_id_prefices=()
 

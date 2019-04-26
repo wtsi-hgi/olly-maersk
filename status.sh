6e60bd59936c314d5fc19b6652e9e616b8aa1b09
Christopher Harrison <ch12@sanger.ac.uk>
2019-04-26 12:07:22 +0100

    Added submission time to LSF job status output and moved execution host to the end

--- status.sh
+++ status.sh
@@ -26,19 +26,19 @@ usage() {
 
 lfs_job_status() {
   # Return the status of an LSF job as a tab-delimited string of LSF
-  # status, execution host, start time and finish time. A dash is used
-  # for any fields where such information is not available, including
-  # jobs which are not found. (Note that jobs that have ended and
-  # dropped out of the LSF log rotation won't be found -- presuming they
-  # existed at all -- their actual status may be available using bhist,
-  # but this is costly and also subject to LSF's log rotation.)
+  # status, submit time, start time, finish time and execution host. A
+  # dash is used for any fields where such information is not available,
+  # including jobs which are not found. (Note that jobs that have ended
+  # and dropped out of the LSF log rotation won't be found -- presuming
+  # they existed at all -- their actual status may be available using
+  # bhist, but this is costly and also subject to LSF's log rotation.)
 
   # n.b., This only works correctly for scalar jobs, but that's all
   # we're dealing with so it's totally fine.
   local job_id="$1"
-  local not_found="-${TAB}-${TAB}-${TAB}-"
+  local not_found="-${TAB}-${TAB}-${TAB}-${TAB}-"
 
-  local headers="stat exec_host start_time finish_time delimiter='${TAB}'"
+  local headers="stat submit_time start_time finish_time exec_host delimiter='${TAB}'"
   local status="$(bjobs -noheader -o "${headers}" "${job_id}" 2>/dev/null)"
 
   echo "${status:-${not_found}}"
@@ -47,6 +47,7 @@ lfs_job_status() {
 # NOTES
 # stdout.background contains the LSF job ID, if it's been submitted
 # rc contains the exit code, if it's ended gracefully
+# stdout or stdout.lsf contains CPU time in seconds
 
 main() {
   if ! (( $# )); then
